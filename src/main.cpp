7867738cb1763bf1ad4fd119cfe8c4699aa13ab2
Anton Chetvertov <antonchetvertkov@gmail.com>
2026-07-08 00:56:48 +0300

    added bmp280 testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,62 @@
-#include "FastIMU.h"
+//General
 #include <Wire.h>
+int DEBUG = 1;
 
+//imu defines
+#include "FastIMU.h"
 #define IMU_ADDRESS 0x68
 #define PERFORM_CALIBRATION
 MPU6500 IMU;
 
+//bmp defines
+#include <Adafruit_BMP280.h>
+Adafruit_BMP280 bmp;
+
+//calibration datasets
 calData calib = { 0 };
 AccelData accelData;
 GyroData gyroData;
 
 void setup() {
+  //I2C, serial, housekeaping stuff
   Wire.begin();
   Wire.setClock(400000);
   Serial.begin(115200);
-  while (!Serial) {
-    ;
-  }
 
-  int err = IMU.init(calib, IMU_ADDRESS);
-  if (err != 0) {
-    Serial.print("Error initializing IMU: ");
-    Serial.println(err);
-    while (true) {
-      ;
-    }
-  }
+  //IMU
+  int imuErr = IMU.init(calib, IMU_ADDRESS);
 
-#ifdef PERFORM_CALIBRATION
-  Serial.println("FastIMU calibration & data example");
-  delay(5000);
-  Serial.println("Keep IMU level.");
-  delay(5000);
-  IMU.calibrateAccelGyro(&calib);
-  Serial.println("Calibration done!");
-  Serial.println("Accel biases X/Y/Z: ");
-  Serial.print(calib.accelBias[0]);
-  Serial.print(", ");
-  Serial.print(calib.accelBias[1]);
-  Serial.print(", ");
-  Serial.println(calib.accelBias[2]);
-  Serial.println("Gyro biases X/Y/Z: ");
-  Serial.print(calib.gyroBias[0]);
-  Serial.print(", ");
-  Serial.print(calib.gyroBias[1]);
-  Serial.print(", ");
-  Serial.println(calib.gyroBias[2]);
-  delay(5000);
-  IMU.init(calib, IMU_ADDRESS);
-#endif
+  if (!DEBUG){
+    Serial.println("FastIMU calibration & data example");
+    delay(5000);
+    Serial.println("Keep IMU level.");
+    delay(5000);
+    IMU.calibrateAccelGyro(&calib);
+    Serial.println("Calibration done!");
+    Serial.println("Accel biases X/Y/Z: ");
+    Serial.print(calib.accelBias[0]);
+    Serial.print(", ");
+    Serial.print(calib.accelBias[1]);
+    Serial.print(", ");
+    Serial.println(calib.accelBias[2]);
+    Serial.println("Gyro biases X/Y/Z: ");
+    Serial.print(calib.gyroBias[0]);
+    Serial.print(", ");
+    Serial.print(calib.gyroBias[1]);
+    Serial.print(", ");
+    Serial.println(calib.gyroBias[2]);
+    delay(5000);
+    IMU.init(calib, IMU_ADDRESS);
+    };
+
+  //bmp
+  bmp.begin(0x76);
+
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,
+              Adafruit_BMP280::SAMPLING_X2,
+              Adafruit_BMP280::SAMPLING_X16,
+              Adafruit_BMP280::FILTER_X16,
+              Adafruit_BMP280::STANDBY_MS_500);
 }
 
 void loop() {
@@ -69,8 +78,16 @@ void loop() {
 	  Serial.print("\t");
 	  Serial.println(IMU.getTemp());
   }
-  else {
-    Serial.println();
-  }
+  Serial.print(F("Temperature = "));
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
+
+  Serial.print(F("Pressure = "));
+  Serial.print(bmp.readPressure());
+  Serial.println(" Pa");
+
+  Serial.print(F("Approx altitude = "));
+  Serial.print(bmp.readAltitude(1013.25));
+  Serial.println(" m");
   delay(50);
 }
