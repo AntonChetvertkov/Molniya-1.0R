dd2b8d9bfe5a0f342f2c889d8aac35d6a6c2e094
Anton Chetvertov <antonchetvertkov@gmail.com>
2026-07-08 00:08:12 +0300

    test mpu : modified FastImu example

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,76 @@
-#include <Arduino.h>
+#include "FastIMU.h"
+#include <Wire.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define IMU_ADDRESS 0x68
+#define PERFORM_CALIBRATION
+MPU6500 IMU;
+
+calData calib = { 0 };
+AccelData accelData;
+GyroData gyroData;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Wire.begin();
+  Wire.setClock(400000);
+  Serial.begin(115200);
+  while (!Serial) {
+    ;
+  }
+
+  int err = IMU.init(calib, IMU_ADDRESS);
+  if (err != 0) {
+    Serial.print("Error initializing IMU: ");
+    Serial.println(err);
+    while (true) {
+      ;
+    }
+  }
+
+#ifdef PERFORM_CALIBRATION
+  Serial.println("FastIMU calibration & data example");
+  delay(5000);
+  Serial.println("Keep IMU level.");
+  delay(5000);
+  IMU.calibrateAccelGyro(&calib);
+  Serial.println("Calibration done!");
+  Serial.println("Accel biases X/Y/Z: ");
+  Serial.print(calib.accelBias[0]);
+  Serial.print(", ");
+  Serial.print(calib.accelBias[1]);
+  Serial.print(", ");
+  Serial.println(calib.accelBias[2]);
+  Serial.println("Gyro biases X/Y/Z: ");
+  Serial.print(calib.gyroBias[0]);
+  Serial.print(", ");
+  Serial.print(calib.gyroBias[1]);
+  Serial.print(", ");
+  Serial.println(calib.gyroBias[2]);
+  delay(5000);
+  IMU.init(calib, IMU_ADDRESS);
+#endif
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  IMU.update();
+  IMU.getAccel(&accelData);
+  Serial.print(accelData.accelX);
+  Serial.print("\t");
+  Serial.print(accelData.accelY);
+  Serial.print("\t");
+  Serial.print(accelData.accelZ);
+  Serial.print("\t");
+  IMU.getGyro(&gyroData);
+  Serial.print(gyroData.gyroX);
+  Serial.print("\t");
+  Serial.print(gyroData.gyroY);
+  Serial.print("\t");
+  Serial.print(gyroData.gyroZ);
+  if (IMU.hasTemperature()) {
+	  Serial.print("\t");
+	  Serial.println(IMU.getTemp());
+  }
+  else {
+    Serial.println();
+  }
+  delay(50);
 }
